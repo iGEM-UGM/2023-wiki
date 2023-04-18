0e2afa5bc74195e13836b1729b369f9630d7cab5
farrelamroe <farrelamroe@gmail.com>
2023-04-18 12:32:20 +0700

    navbar + footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,29 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        100: "#52a0f3",
+        200: "#125bb8",
+        300: "#252644",
+      },
+      secondary: "#f0d8b4",
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    display: ["responsive", "group-hover", "group-focus"],
   },
   plugins: [],
-}
+};
