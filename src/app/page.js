5175c3c3261d215ab9fa130ac692fb736e4f5ac3
farrelamroe <farrelamroe@gmail.com>
2023-04-21 00:33:32 +0700

    navbar + footer

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+     <Navbar />
+     <Footer />
+    </>
+  );
+}
